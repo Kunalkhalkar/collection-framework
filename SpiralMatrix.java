5a148ee8116ebeda4193731da215e30bb14ed03f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-12-29 23:04:29 +0530

    initial commit

---
+++ SpiralMatrix.java
@@ -0,0 +1,16 @@
+/*
+6. Spiral Matrix
+Description:
+Return all elements of a matrix in spiral order using ArrayList.
+Example:
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+*/
+import java.util.*;
+public class SpiralMatrix{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Elements in matrix");
+		ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+	}
+}
