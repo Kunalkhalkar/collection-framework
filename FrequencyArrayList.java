453703bdf0137020c929ff9ba378df3abd19c6f7
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-12-29 23:03:56 +0530

    initial commit

---
+++ FrequencyArrayList.java
@@ -0,0 +1,20 @@
+/*Q4. Take an integer array with repeated values. Store it in an ArrayList and print the frequency of each unique number.
+What you practice:
+Traversing ArrayList
+Using another ArrayList to track visited elements
+Logical counting
+*/
+import java.util.*;
+public class FrequencyArrayList{
+	public static void main(String ...x){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter repeated element in array");
+		int a[] = new int[6];
+		ArrayList<Integer> al = new ArrayList<>();
+		
+		for(int i= 0; i<a.length; i++){
+			a[i] = sc.nextInt();
+		}
+		
+	}
+}
