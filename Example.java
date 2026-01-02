a4ff7b88251ff24df483e215045c6484face90c9
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-02 22:52:28 +0530

    new

---
+++ Example.java
@@ -0,0 +1,12 @@
+public class Example
+{
+public static void main(String x[])
+{
+	Example e=new Example();
+	e.display();
+}
+public  void display()
+{
+System.out.println("I am display");
+}
+}
--- arralist.java
+++ arralist.java
@@ -1,2 +1,3 @@
 /*
+array List program
 */
