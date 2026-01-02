8d61330497541f5f9c4a613464ec9e82e25338cc
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-02 22:51:45 +0530

    spiral

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -5,6 +5,12 @@ Return all elements of a matrix in spiral order using ArrayList.
 Example:
 Input: [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
+  0 
+  
+  0 1 2
+0 1 2 3
+1 4 5 6 
+2 7 8 9
 */
 import java.util.*;
 public class SpiralMatrix{
