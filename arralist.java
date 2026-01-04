a3e5a1ee54df44e6a4a030431be062234c5c3806
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-04 21:35:23 +0530

    new

--- arralist.java
+++ arralist.java
@@ -1,3 +1,15 @@
 /*
 array List program
 */
+import java.util.*;
+public class arralist{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the element in array List");
+		ArrayList<Integer> al = new ArrayList<>();
+		
+		for(int i=0; i<5; i++){
+			al.add(sc.nextInt());
+		}
+	}
+}
